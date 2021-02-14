8ef6a4d3aa338957e3a06236669af7cafd2b9b99
Enrick Rico <e.ficadiere@gmail.com>
2021-02-14 19:08:09 +0100

    exception pour methode creerRef() pour employe

---
+++ src/webApp/exception/RefInvalidException.java
@@ -0,0 +1,11 @@
+package webApp.exception;
+
+public class RefInvalidException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	public RefInvalidException(String msgExcpEmploye) {
+		super(msgExcpEmploye +"**");
+	}
+
+}
